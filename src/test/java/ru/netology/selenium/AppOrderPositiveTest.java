afd25b5b6d08e6722f2f29b1fe0b8d1c1a49a4bf
Helga NN <o9351030@gmail.com>
2026-04-23 21:30:50 +0300

    WebDriver problem

---
+++ src/test/java/ru/netology/selenium/AppOrderPositiveTest.java
@@ -0,0 +1,52 @@
+package ru.netology.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppOrderPositiveTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--desable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldBeSuccessfulForm() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Петр");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79512345290");
+        driver.findElement(By.cssSelector("[data-test-id='agreement'] input")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement actualElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        String actualText = actualElement.getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с Вами в ближайшее время.", actualText);
+        assertTrue(actualElement.isDisplayed());
+    }
+}
